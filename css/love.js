8d36336efe086e35dc71ddd270e7724cb2a9442f
向鹏龙 <itxiangpenglong@163.com>
2023-08-21 14:29:47 +0800

    去掉敏感信息

--- css/love.js
+++ css/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 楷体 ,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("梁思思★亲启", 23, 10);
+            ctx.fillText("XXXX★亲启", 23, 10);
             ctx.restore();
         },
         clear: function() {
